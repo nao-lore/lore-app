25276a47d38d4cda7e36cde01dd69c2d7aa91e72
nao-lore <262077168+nao-lore@users.noreply.github.com>
2026-04-04 01:14:43 +0900

    session end: 2026-04-04 01:14

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Lore development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            nodePackages.npm
+            nodePackages.typescript
+            git
+            gh
+          ];
+
+          shellHook = ''
+            echo "🔧 Lore dev environment loaded (Node $(node --version))"
+          '';
+        };
+      });
+}
